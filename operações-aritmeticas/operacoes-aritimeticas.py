7ad697ebf829a3243efeaad5fd7279383f2113ca
amanjonson22 <amandacsr10@gmail.com>
2023-09-01 12:15:12 -0300

    operações aritmeticas

---
+++ operações-aritmeticas/operacoes-aritimeticas.py
@@ -0,0 +1,20 @@
+# Adição
+print('Adição: 15 + 20 =', 15 + 20)
+
+# Subtração
+print('Subtração: 12 - 2 =', 12 - 2)
+
+# Multiplicação
+print('Multiplicação: 22 * 3 =', 22 * 3)
+
+# Divisão
+print('Divisão: 25/5 =', 25/5)
+
+# Divisão inteira
+print('Divisão inteira: 25//5 =', 25//5)
+
+# Módulo (ou resto de divisão)
+print('O resto da divisão: 25%2 =', 25%2)
+
+# Exponenciação
+print('Exponencial: 2 ** 3 =', 2**3)
---
+++ operações-aritmeticas/operadores_aritmeticos.py
@@ -0,0 +1,19 @@
+produto_1 = 10
+produto_2 = 20
+produto_3 = 30
+
+juros = 0.05
+
+print(((produto_1+produto_2+produto_3)*juros)+(produto_1+produto_2+produto_3))
+print((produto_1+produto_2+produto_3)-((produto_1+produto_2+produto_3)*juros))
+print(produto_3/produto_1)
+print(produto_3//produto_1)
+print(produto_3 * juros)
+print(produto_3%produto_2)
+print(produto_2**produto_1)
+
+x = 10 + 5 * 2
+print(x)
+
+y = (10+5)*2
+print(y)

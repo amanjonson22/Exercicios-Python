e924f6f3256b06e7bec4824e66590090a4fad821
amanjonson22 <amandacsr10@gmail.com>
2023-09-01 11:52:16 -0300

    print e input

---
+++ print-input/print-imput.py
@@ -0,0 +1,8 @@
+nome = input('Informe o seu nome: ')
+idade = input('Informe sua idade: ')
+
+print(nome, idade)
+print(nome, idade, end=' - ')
+print(nome, idade, sep='#')
+print(nome, idade, sep='#', end='.....')
+print(idade, nome)

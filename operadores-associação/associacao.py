7f5f7760f6c5838623d8482753f51603ab267a4a
amanjonson22 <amandacsr10@gmail.com>
2023-09-01 13:04:51 -0300

    codigo de operadores de associação e de atribuição

---
+++ operadores-associação/associacao.py
@@ -0,0 +1,15 @@
+curso = 'Curso de Python'
+frutas = ['laranja','uva','limão','morango']
+saques = [1500, 100]
+
+duvida = 'Python' in curso
+print(duvida)
+
+duvida = 'maçã' in frutas
+print(duvida)
+
+duvida = 'arroz' not in frutas
+print(duvida)
+
+duvida = 200 in saques
+print(duvida)
---
+++ operadores-de-atribuição/atribuição.py
@@ -0,0 +1,33 @@
+#Atribuindo valores
+
+#O sinal de igual atribui valor
+saldo = 500
+print(saldo)
+
+#O sinal de += ou -= atribui um novo valor somando/subtraindo o antigo ao novo
+saldo += 200
+print(saldo)
+
+saldo -= 150
+print(saldo)
+
+#outros sinais de atribuição
+saldo = 100
+saldo *= 22
+print(saldo)
+
+saldo = 150
+saldo /= 5
+print(saldo)
+
+saldo = 200
+saldo //= 20
+print(saldo)
+
+saldo = 80
+saldo **= 3
+print(saldo)
+
+saldo = 2000
+saldo %= 30
+print(saldo)

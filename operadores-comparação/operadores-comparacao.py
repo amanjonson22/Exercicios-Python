3c4dd6834aff88fc58579aa48e6f3fb65b4840d9
amanjonson22 <amandacsr10@gmail.com>
2023-09-01 12:23:57 -0300

    operadores de comparação

---
+++ operadores-comparação/operadores-comparacao.py
@@ -0,0 +1,14 @@
+saldo = 450
+saque = 200
+
+print(f'saldo: {saldo}\nsaque: {saque}\n')
+
+print('comparadores maior/menor e maior igual a/menor igual a\n')
+print(f'saldo({saldo}) > saque({saque}):', saldo > saque)
+print(f'saldo({saldo}) >= saque({saque}):', saldo >= saque)
+print(f'saldo({saldo}) < saque({saque}):', saldo < saque)
+print(f'saldo({saldo}) <= saque({saque}):', saldo <= saque, end='\n\n')
+
+print(f'comparadores de diferente de/igual a\n')
+print(f'saldo({saldo}) != saque({saque}):',saldo != saque)
+print(f'saldo({saldo}) == saque({saque}):',saldo == saque, end='\n\n')

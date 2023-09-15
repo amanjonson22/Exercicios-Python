34140f1f9c27940cf73b93a754418dfea4196e43
amanjonson22 <amandacsr10@gmail.com>
2023-09-14 23:20:01 -0300

    exercicio 8 e 9

---
+++ programas/exercicios funções/exercicio8.py
@@ -0,0 +1,6 @@
+#Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado
+def quantidade_dígitos():
+    numero = input('Informe um número: ')
+    print(f'O número {numero} tem {len(numero)} dígitos.')
+
+quantidade_dígitos()

0153ee0ba4ab67be7ee683e1a004909e7e82d366
Amanda Caroline <amandacsr10@gmail.com>
2023-09-28 23:54:05 -0300

    edit dos códigos

--- Programação-Orientada-Objetos/polimorfismo/01_polimorfismo.py
+++ Programação-Orientada-Objetos/polimorfismo/01_polimorfismo.py
@@ -1,16 +1,26 @@
 class Passaro:
-    def voar(self): pass
+    def voar(self): 
+        print("Voando...")
 
 class Pardal(Passaro): 
     def voar(self):
-        print("Pardal voa")
+        super().voar()
+        #print("Pardal voa")
 
 class Avestruz(Passaro):
     def voar(self):
         print("Avestruz não voa")
 
-def plano_de_voo(passaro):
-    passaro.voar()
+# FIXME: exemplo ruim do uso de herança para "ganhar" o método voar
+class Aviao(Passaro):
+    def voar(self):
+        print("Avião está decolando")
+
+#O conceito de polimorfismo está aqui
+def plano_de_voo(obj):
+    obj.voar()
+
 
 plano_de_voo(Pardal())
 plano_de_voo(Avestruz())
+plano_de_voo(Aviao())

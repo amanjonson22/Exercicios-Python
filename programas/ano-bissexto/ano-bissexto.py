dff39ac9dc0ef2ed6ce444bfb107fd8163b8b884
Amanda Caroline <amandacsr10@gmail.com>
2023-10-06 12:48:45 -0300

    edit ano-bissexto

--- programas/ano-bissexto/ano-bissexto.py
+++ programas/ano-bissexto/ano-bissexto.py
@@ -1,12 +1,14 @@
 while True:
-    escolha = input("Você quer continuar checando?\n[s] Sim\n[n] Não").lower()
+    escolha = input("Você quer continuar checando?\n[s] Sim\n[n] Não\n=>").lower()
     if escolha == 's':
-        ano = int(input('Qual o ano? '))
+        ano = int(input('\nQual o ano? '))
         resultado = True
 
         resultado = (ano % 4 == 0) and (ano % 100 > 0) or (ano % 400 == 0)
-        print(f'O ano {ano} é bissexto? {"sim" if resultado == True else "não"}')
+        print(f'\nO ano {ano} é bissexto? {"sim" if resultado == True else "não"}')
     
-    else:
-        print("Entendido! Até a próxima!")
+    elif escolha == 'n':
+        print("\nEntendido! Até a próxima!")
         break
+
+    else: print('\nNão entendi...')

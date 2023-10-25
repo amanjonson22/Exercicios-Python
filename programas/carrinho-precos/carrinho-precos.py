18d3bbf35ca94952e296d53f69b5a86391e9cd66
Amanda Caroline <amandacsr10@gmail.com>
2023-10-25 17:41:27 -0300

    create and edit carrinho-precos.py

---
+++ programas/carrinho-precos/carrinho-precos.py
@@ -0,0 +1,23 @@
+itens = []
+precos = []
+total = 0
+
+
+menu = int(input('Quantos itens no seu carrinho de compras? \n'))
+
+for i in range(menu):
+    item = input('Digite o item:\n')
+    preco = float(input('Digite o preço:\n'))
+
+    itens.append(item)
+    precos.append(preco)
+
+print('\nCalculando seu total...\n')
+
+for i in range(len(precos)):
+    total += precos[i]
+
+for i in range(len(itens)):
+    print(itens[i] + ':', precos[i])
+
+print(f'\nSeu total é: {total}')
